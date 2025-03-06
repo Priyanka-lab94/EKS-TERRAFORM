83997a647509305f8a9642991d3b35a5542d2af5
Priyanka-lab94 <priyalog2020@gmail.com>
2025-03-06 19:49:39 +0530

    Change2

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "myawsbucker-terraform" # Replace with your actual S3 bucket name
+    bucket = "myawsbucker-terraformproject" # Replace with your actual S3 bucket name
     key    = "Eks/terraform.tfstate"
     region = "us-east-1"
   }
